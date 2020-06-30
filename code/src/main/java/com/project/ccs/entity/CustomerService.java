1bf75073348175e0753ef58d0d9cc34b73b81ccc
TranpriMit <TranpriMit@gmail.com>
2020-06-30 17:42:54 +0800

    创建cusomer_service类

--- code/src/main/java/com/project/ccs/entity/CustomerService.java
+++ code/src/main/java/com/project/ccs/entity/CustomerService.java
@@ -2,9 +2,27 @@ package com.project.ccs.entity;
 
 import lombok.Data;
 
+import javax.xml.ws.Service;
+import java.sql.Timestamp;
 import java.util.Date;
 
 @Data
 public class CustomerService {
-
+    private String phone;
+    private String password;
+    private int state;
+    private String headImg;
+    private String email;
+    private String niceName;
+    private String realname;
+    private int customerServiceId;
+    private String customerServicePhone;
+    private Timestamp totalOnlineTime;
+    private Timestamp onlineTimeBegin;
+    private Timestamp onlineTimeEnd;
+    private int sessionCount;
+    private int msgCount;
+    private int customerServiceGroupId;
+    private String role;
+    private int serviceCap;
 }
