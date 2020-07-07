32e4278b8a495d77e0dd80930f0a094e3e718d6b
TranpriMit <TranpriMit@gmail.com>
2020-07-07 19:23:51 +0800

    element-ui安装

---
+++ try/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-table :data="data_list">
+     <el-table-column  :label="date" v-for="(date, key) in header">
+             <template scope="scope">
+                   {{data_list[scope.$index][key]}}
+              </template>
+       </el-table-column>
+    </el-table>
+  </template>
+ <script>
+      export default {
+      data() {
+            return {
+                header:["column1","column2", "column3"],
+                data_list:[
+                    [1, 2, 3],
+                    [4,5,6]
+                ]
+            }
+        }
+    }
+ </script>
--- ync_html/src/App.vue
+++ ync_html/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<visitorls></visitorls>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import visitorls from './components/visitorls.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    visitorls
   }
 }
 </script>
